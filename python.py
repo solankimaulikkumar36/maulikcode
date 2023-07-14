ab86544eef1224e4a452a801717d07e8a4a4b5f9
maulik <solankimaulikkumar36@gmail.com>
2023-07-14 08:53:20 +0530

    (this is factorial)

---
+++ python.py
@@ -0,0 +1,11 @@
+#n=int(input("Enter the number"));
+#for i in range n:
+#print(n*i);
+n=int(input("Enter the number"));
+fact=1
+
+for i in range(n):  
+                     fact=fact*(i+1)
+                     
+print(fact); 
+
